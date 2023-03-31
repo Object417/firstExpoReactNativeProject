5f519130696cbececcd04d88ef5d73d716411c0a
Object_417 <belovedofthelord1@gmail.com>
2023-03-31 13:37:00 +0100

    fixed hiragana screen

--- src/screens/Hiragana/index.jsx
+++ src/screens/Hiragana/index.jsx
@@ -5,10 +5,17 @@ import {
   hiraganaBaseSelector
 } from "@/store/hiraganaSlice"
 import { Audio } from "expo-av"
+import { StatusBar } from "expo-status-bar"
 import _ from "lodash"
 
 import { useEffect, useState } from "react"
-import { Dimensions, ScrollView, StyleSheet, View } from "react-native"
+import {
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  View,
+  ScrollView
+} from "react-native"
 import { Card, Text } from "react-native-paper"
 import { useSelector } from "react-redux"
 
@@ -16,20 +23,20 @@ function HiraganaScreen() {
   const styles = StyleSheet.create({
     title: {},
     container: {
-      flexDirection: "row",
-      flexWrap: "wrap",
-      gap: 8,
-      marginTop: 8,
-      marginBottom: 16
+      marginBottom: 16,
+      paddingHorizontal: 16
     },
     symbolCard: {
       flexGrow: 1,
+      margin: 4,
+      // aspectRatio: 1 / 1,
+      maxWidth: (Dimensions.get("window").width - 32) / 5 - (5 * 8) / 5
       // workaround. I don't know why it calculates incorrectly
-      flexBasis: (Dimensions.get("window").width - 32) / 5 - 8,
-      maxWidth: (Dimensions.get("window").width - 32) / 5 - 6
+      // flexBasis: (Dimensions.get("window").width - 32) / 5 - 8,
+      // maxWidth: (Dimensions.get("window").width - 32) / 5 - 6
     },
     symbolCardContainer: {
-      padding: "8px"
+      // padding: 8
     },
     symbolCardText: {
       textAlign: "center"
@@ -64,34 +71,72 @@ function HiraganaScreen() {
   }, [sound])
 
   return (
-    <ScrollView style={{ paddingHorizontal: 16 }}>
-      {hiraganaPlus.map(({ title, symbols }) => (
-        <View key={title}>
-          <Text variant="titleLarge" style={styles.title}>
-            {title}
-          </Text>
-          <View style={styles.container}>
-            {symbols.map(({ letter, symbol }) => (
+    <>
+      <StatusBar style="light" />
+      <FlatList
+        data={hiraganaPlus}
+        keyExtractor={(item) => item.title}
+        style={styles.container}
+        renderItem={({ item, index, separators }) => (
+          <FlatList
+            data={item.symbols}
+            horizontal={false}
+            numColumns={5}
+            keyExtractor={(item) => item.letter}
+            ListHeaderComponent={<Text variant="titleLarge">{item.title}</Text>}
+            ListHeaderComponentStyle={{ marginTop: 16, marginBottom: 4 }}
+            columnWrapperStyle={{}}
+            renderItem={({ item, index, separators }) => (
               <Card
-                key={letter}
-                style={styles.symbolCard}
-                onPress={() => handleCardPress(letter)}
+                mode="elevated"
+                onPress={(e) => handleCardPress(item.letter)}
+                style={{
+                  ...styles.symbolCard
+                }}
               >
-                <Card.Content style={{ padding: 8 }}>
+                <Card.Content style={styles.symbolCardContainer}>
                   <Text variant="titleLarge" style={styles.symbolCardText}>
-                    {symbol}
+                    {item.symbol}
                   </Text>
-                  <Text variant="bodyMedium" style={styles.symbolCardText}>
-                    {letter}
+                  <Text variant="bodyLarge" style={styles.symbolCardText}>
+                    {item.letter}
                   </Text>
                 </Card.Content>
               </Card>
-            ))}
-          </View>
-        </View>
-      ))}
-    </ScrollView>
+            )}
+          />
+        )}
+      />
+    </>
   )
+
+  // <ScrollView style={{ paddingHorizontal: 16 }}>
+  //   {hiraganaPlus.map(({ title, symbols }) => (
+  //     <View key={title}>
+  //       <Text variant="titleLarge" style={styles.title}>
+  //         {title}
+  //       </Text>
+  //       <View style={styles.container}>
+  //         {symbols.map(({ letter, symbol }) => (
+  //           <Card
+  //             key={letter}
+  //             style={styles.symbolCard}
+  //             onPress={() => handleCardPress(letter)}
+  //           >
+  //             <Card.Content style={{ padding: 8 }}>
+  //               <Text variant="titleLarge" style={styles.symbolCardText}>
+  //                 {symbol}
+  //               </Text>
+  //               <Text variant="bodyMedium" style={styles.symbolCardText}>
+  //                 {letter}
+  //               </Text>
+  //             </Card.Content>
+  //           </Card>
+  //         ))}
+  //       </View>
+  //     </View>
+  //   ))}
+  // </ScrollView>
 }
 
 export default HiraganaScreen
